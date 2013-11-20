736e56250072228f0708c0823d00608b5c88c7a2
Benjamin Rokseth <bensinober@gmail.com>
2013-11-20 13:59:39 +0100

    fixes and updates

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ module.exports.index = function(req, res){
 
 
   var holdsrequest = request.get(config.opac+"/cgi-bin/koha/rest.pl/holds/all", function(err, response, body) {
-    console.log(req.params)
+    console.log(req.params);
     if (!err && response.statusCode == 200) {
       var json = JSON.parse(body);
       res.render('index', { layout: true, title: 'Plukkliste', books: json });
@@ -23,27 +23,24 @@ module.exports.index = function(req, res){
 }
 
 module.exports.foundBook = function(req, res){
-  console.log(req.params)
+  console.log(req.params);
   // var options = {
   //   host: 'localhost',
   //   port: 8080,
   //   path: "http://localhost:8080/cgi-bin/koha/rest.pl/holds/"+req.params.biblionumber+"/"+req.params.borrowernumber+"/found_book",
   //   method: 'PUT'
   // };
-
-  var bookrequest = request.put(config.opac+"/cgi-bin/koha/rest.pl/holds/"+
-      req.params.biblionumber+"/"+
-      req.params.itemnumber+"/"+
-      req.params.borrowernumber+
-      "/found_book", function(err, response, body) {
+  var url = config.opac+"/cgi-bin/koha/rest.pl/holds/"+req.params.biblionumber+"/"+req.params.itemnumber+"/"+req.params.borrowernumber+"/found_book";
+  console.log(url);
+  var bookrequest = request.put(url, function(err, response, body) {
     if (!err && response.statusCode == 200) {
       var json = JSON.parse(body);
-      console.log("response"+json)
+      console.log("response: "+json);
       res.json(json);
     } else if (!err) {
-      console.log("error" +err);
+      console.log("error: " +err);
     } else {
-      console.log("body" +body);
+      console.log("body: " +body);
     }
 
   });
